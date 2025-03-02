7e168c22df86908161bdedb1142697149f04b6a4
SurajSakhare100 <sakharesuraj10@gmail.com>
2025-03-02 13:41:56 +0530

    file str added

---
+++ constants/index.ts
@@ -0,0 +1,35 @@
+import onboarding1 from "@/assets/images/onboarding1.svg";
+import onboarding2 from "@/assets/images/onboarding2.svg";
+import onboarding3 from "@/assets/images/onboarding3.svg";
+import person from "@/assets/icons/person.png";
+import email from "@/assets/icons/email.png";
+import lock from "@/assets/icons/lock.png";
+import google from "@/assets/icons/google.png";
+
+export const onboarding = [
+    {
+        id: 1,
+        title: "Create Your Dream Trip",
+        description: "Easily plan personalized itineraries with must-visit spots, activities, and seamless scheduling.",
+        image: onboarding1
+    },
+    {
+        id: 2,
+        title: "Find Hidden Treasures",
+        description: "Discover unique local gems, offbeat experiences, and the best-kept secrets of every destination.",
+        image: onboarding2
+    },
+    {
+        id: 3,
+        title: "Stay Organized & Enjoy",
+        description: "Keep all your trip details in one place, so you can focus on the adventure and leave the stress behind.",
+        image: onboarding3
+    }
+];
+
+export const icons = {
+    person,
+    email,
+    lock,
+    google
+};
